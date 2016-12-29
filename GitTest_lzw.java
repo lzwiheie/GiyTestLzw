7b1ee62d99fc16785613a139bec29635f7c3fe52
aaaa <aaaa@163.com>
2016-12-29 10:56:46 +0800

    又增加了b

--- GitTest_lzw.java
+++ GitTest_lzw.java
@@ -3,5 +3,7 @@ package com.atguigu;
 public class GitTest_lzw {
 	public static void main(String[] args) {
 		System.out.println("aaaaaaaaaaaaaaaaaaa");
+		System.out.println("bbbbbbbbbbbbbbbbbbbbb");
+		
 	}
 }
