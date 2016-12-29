961069d6d423b49c7c887d079f0f7d8f8b53627a
aaaa <aaaa@163.com>
2016-12-29 11:37:00 +0800

    试试

--- GitTest_lzw.java
+++ GitTest_lzw.java
@@ -3,7 +3,8 @@ package com.atguigu;
 public class GitTest_lzw {
 	public static void main(String[] args) {
 		System.out.println("aaaaaaaaaaaaaaaaaaa");
-		System.out.println("bbbbbbbbbbbbbbbbbbbbb");
-		
+		System.out.println("bbbbbbbbbbbbbbbbbbb");
+		System.out.println("ccccccccccccccccccc");
+		System.out.println("dddddddddddddddddddd");
 	}
 }
